27778875f451bed683c4000a4978e017d82e2d20
marianordqvist <marianordqvist@live.se>
2023-12-19 13:48:30 +0100

    initial

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+
+function App() {
+  return (
+    <>
+  <Header></Header>
+  <Food></Food>
+  <Food></Food>
+  <Food></Food>
+  <Footer></Footer>
+    </>
+  )
+}
+
+export default App;
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+return(
+    <footer>
+        <p>&copy; {new Date().getFullYear()} Website name</p>
+    </footer>
+)}
+
+export default Footer
